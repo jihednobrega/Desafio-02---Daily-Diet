b25c3cf65b37ed220dc6d8f7de2bd18a7f31021d
Jihed Nóbrega <jihednobrega@gmail.com>
2024-12-17 11:56:35 -0300

    database files config finished

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,19 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
+import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
-app.get('/', () => {
-  return { message: 'Hello, World!' }
+app.get('/users', async () => {
+  const users = await knex('users').where('name', 'John Doe').select('*')
+
+  return users
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('Server is running on port 3333')
